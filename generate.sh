a7ad732a92c348d48377641f2892eeb9ac7b6843
Marek Goldmann <marek.goldmann@gmail.com>
2015-10-22 16:17:19 +0200

    Use template instead of the whole directory, add more output messages

--- generate.sh
+++ generate.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 VERSION=${VERSION:="1.0.0"}
-INPUT_DIR=${INPUT_DIR:="`pwd`"}
-SCRIPTS_DIR=${SCRIPTS_DIR:="$INPUT_DIR/scripts"}
-OUTPUT_DIR=${OUTPUT_DIR:="$INPUT_DIR/target"}
+TEMPLATE=${TEMPLATE:="image.yaml"}
+SCRIPTS_DIR=${SCRIPTS_DIR:="`pwd`/scripts"}
+OUTPUT_DIR=${OUTPUT_DIR:="`pwd`/target"}
 
 # Pre-create the target directory
 mkdir -p $OUTPUT_DIR
 
-docker run -t --rm -v $SCRIPTS_DIR:/scripts:z -v $INPUT_DIR:/input:z -v $OUTPUT_DIR:/output:z jboss/dockerfile-generator:$VERSION
+echo "Using template: $TEMPLATE"
+echo "Using scripts directory: $SCRIPTS_DIR"
+echo "Using output directory: $OUTPUT_DIR"
+
+docker run -t --rm -v $SCRIPTS_DIR:/scripts:z -v $TEMPLATE:/input/image.yaml:z -v $OUTPUT_DIR:/output:z jboss/dockerfile-generator:$VERSION
 
