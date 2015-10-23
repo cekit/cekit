ce7c159ad313c7f37c28d4fec8d8e3737906063b
Marek Goldmann <marek.goldmann@gmail.com>
2015-10-23 12:29:38 +0200

    Do not require scripts directory to be mounted, better logging

--- generate.sh
+++ generate.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 
 VERSION=${VERSION:="1.0.0"}
+echo "Using '$VERSION' version of the generator tool"
+
 TEMPLATE=${TEMPLATE:="`pwd`/image.yaml"}
-SCRIPTS_DIR=${SCRIPTS_DIR:="`pwd`/scripts"}
+echo "Using '$TEMPLATE' template"
+
 OUTPUT_DIR=${OUTPUT_DIR:="`pwd`/target"}
+echo "Using '$OUTPUT_DIR' as the output directory"
+
+# If SCRIPTS_DIR env variable is not provided,
+# check if the default location (currentdir/scripts)
+# exists, if yes, assume that this is the scripts
+# directory to mount
+if [ -z "$SCRIPTS_DIR" ] && [ -d "`pwd`/scripts" ]; then
+    SCRIPTS_DIR=${SCRIPTS_DIR:="`pwd`/scripts"}
+fi
+
+volumes="-v $TEMPLATE:/input/image.yaml:z -v $OUTPUT_DIR:/output:z"
+
+if [ -n "$SCRIPTS_DIR" ];
+    echo "Using '$SCRIPTS_DIR' as the scripts directory"
+    volumes="$volumes -v $SCRIPTS_DIR:/scripts:z"
+fi
 
 # Pre-create the target directory
 mkdir -p $OUTPUT_DIR
 
-echo "Using template: $TEMPLATE"
-echo "Using scripts directory: $SCRIPTS_DIR"
-echo "Using output directory: $OUTPUT_DIR"
-
-exec docker run -t --rm -v $SCRIPTS_DIR:/scripts:z -v $TEMPLATE:/input/image.yaml:z -v $OUTPUT_DIR:/output:z jboss/dockerfile-generator:$VERSION
+exec docker run -t --rm $volumes jboss/dockerfile-generator:$VERSION
 
