1d2e17676f13ab4a0223d4556d9d8bb919b21301
Marek Goldmann <marek.goldmann@gmail.com>
2015-10-30 15:15:19 +0100

    Do not fail when the provided descriptor fail is owned by root

--- launch.sh
+++ launch.sh
@@ -8,9 +8,14 @@ if [ "$#" -lt 2 ]; then
 fi
 
 # Assume that the path to the descriptor file is second from the end
-stat=(`stat -c "%u %g" ${@: -2:1}`)
+stat=(`stat -c "%g %u" ${@: -2:1}`)
 
-groupadd -r dogen -g ${stat[1]} && useradd -u ${stat[0]} -r -g dogen -M -d /opt/dogen dogen
+if [ "${stat[0]}" != "0" ] && [ "${stat[1]}" != "0" ]; then
+    groupadd -r dogen -g ${stat[0]}
+    useradd -u ${stat[1]} -r -g dogen -M -d /opt/dogen dogen
+    runuser dogen -c "/usr/bin/dogen $*"
+else
+    /usr/bin/dogen "$@"
+fi
 
-runuser dogen -c "/usr/bin/dogen $*"
 
