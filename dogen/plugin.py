b06615d55802ed79e0f7a005aa5652a896b92269
Marek Goldmann <marek.goldmann@gmail.com>
2016-04-18 13:58:06 +0200

    Initial plugin architecture + conversion of dist-git feature to a plugin

---
+++ dogen/plugin.py
@@ -0,0 +1,17 @@
+
+class Plugin(object):
+    @classmethod
+    def list(cls):
+        return cls.__subclasses__()
+
+    def __init__(self, dogen):
+        self.dogen = dogen
+        self.log = dogen.log
+        self.descriptor = dogen.descriptor
+        self.output = dogen.output
+        
+    def prepare(self):
+        pass
+
+    def after_sources(self):
+        pass
