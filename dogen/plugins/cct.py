c3d610472708cd95054c4cb3447023051e2e0479
Jonathan Dowland <jdowland@redhat.com>
2016-08-18 15:38:27 +0100

    rename hash/chksum -> md5sum in more places

--- dogen/plugins/cct.py
+++ dogen/plugins/cct.py
@@ -105,7 +105,7 @@ class CCT(Plugin):
         for source in cct_sources:
             dogen_source = {}
             dogen_source['url'] = source_prefix + source['name']
-            dogen_source['hash'] = source['chksum']
+            dogen_source['md5sum'] = source['md5sum']
             dogen_sources.append(dogen_source)
         try:
             cfg['sources'].extend(dogen_sources)
