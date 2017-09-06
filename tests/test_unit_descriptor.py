066eaabf589cec55a329f99cb60ea45cdadcae17
Marek Goldmann <marek.goldmann@gmail.com>
2017-09-06 16:37:09 +0200

    Better processing of important labels
    Labels description and summary are set up correctly
    based on the description key in the image descriptor.
    
    See code for more information.
    
    Fixes #12

--- tests/test_unit_descriptor.py
+++ tests/test_unit_descriptor.py
@@ -1,4 +1,7 @@
+import os
+import tempfile
 import unittest
+import yaml
 
 from concreate import descriptor
 from concreate.errors import ConcreateError
@@ -78,3 +81,57 @@ class TestMergingLists(unittest.TestCase):
 
         self.assertEqual(expected,
                          descriptor.merge_lists(list1, list2))
+
+
+class TestLabels(unittest.TestCase):
+
+    def setUp(self):
+        _, self.descriptor = tempfile.mkstemp()
+
+    def tearDown(self):
+        os.remove(self.descriptor)
+
+    def prepare_descriptor(self, data={}):
+        image = {'name': 'image/name', 'version': 1.0,
+                 'from': 'from/image', 'schema_version': 1}
+        image.update(data)
+
+        with open(self.descriptor, 'w') as outfile:
+            yaml.dump(image, outfile, default_flow_style=False)
+
+    def test_no_labels_should_be_added(self):
+        self.prepare_descriptor()
+
+        img_descriptor = descriptor.Descriptor(self.descriptor, 'image')
+        img_descriptor._process_labels()
+
+        self.assertIsNone(img_descriptor.label('description'))
+        self.assertIsNone(img_descriptor.label('summary'))
+        self.assertIsNone(img_descriptor.label('maintainer'))
+
+    def test_description_label_should_be_added(self):
+        self.prepare_descriptor({'description': 'This is image description'})
+
+        img_descriptor = descriptor.Descriptor(self.descriptor, 'image')
+        img_descriptor._process_labels()
+
+        self.assertIsNone(img_descriptor.label('maintainer'))
+        self.assertEqual(img_descriptor.label('description').get(
+            'value'), 'This is image description')
+        # In this case the 'summary' label should be also set
+        self.assertEqual(img_descriptor.label('summary').get(
+            'value'), 'This is image description')
+
+    def test_description_and_summary_labels_should_not_be_overriden(self):
+        self.prepare_descriptor({'description': 'This is image description', 'labels': [
+                                {'name': 'summary', 'value': 'summary value'},
+                                {'name': 'description', 'value': 'description value'}]})
+
+        img_descriptor = descriptor.Descriptor(self.descriptor, 'image')
+        img_descriptor._process_labels()
+
+        self.assertIsNone(img_descriptor.label('maintainer'))
+        self.assertEqual(img_descriptor.label(
+            'description').get('value'), 'description value')
+        self.assertEqual(img_descriptor.label(
+            'summary').get('value'), 'summary value')
