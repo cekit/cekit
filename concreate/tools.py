7e966bfeb2755150c0f916ba641896358576c6f3
Marek Goldmann <mgoldman@redhat.com>
2017-09-15 12:18:31 +0200

    Refactoring
    The 'modules' section was reworked. A new 'repositories' section was
    added (which replaced the top level 'dependencies') and another
    'install' was added to be able to list all modules that need to be
    "installed/processed" to generate the image.
    
    Specifying `cache_url` in configuration file was moved from `artifact`
    to `common` section.
    
    For resources, if you specify only `path` to resource and at the same time
    the `cache_url` will be set in configuration file - Concreate will still
    try to fetch it from cacher, even if it was defined as 'local' path.
    
    Dropped using 'dependencies' word across the code in favor of 'module repositories'.
    
    Resource classes were moved to resource.py. Same happened to unit tests
    for these classes.
    
    Cleaned up logging across the tool.
    
    The `prepare_external_repositories` method code from `tools.py`
    was moved to generator.py and combined with `prepare_repositories`.
    
    Handling of additional repository files is now done via resources too.
    
    Signed-off-by: Marek Goldmann <mgoldman@redhat.com>

--- concreate/tools.py
+++ concreate/tools.py
@@ -5,18 +5,12 @@ except:
 import logging
 import os
 import shutil
-import requests
-try:
-    import urllib.parse as urlparse
-except:
-    import urlparse
 import yaml
 from pykwalify.core import Core
 
 from concreate.errors import ConcreateError
 
 
-SUPPORTED_HASH_ALGORITHMS = ['sha256', 'sha1', 'md5']
 logger = logging.getLogger('concreate')
 
 cfg = {}
@@ -26,50 +20,6 @@ def parse_cfg():
     cp = configparser.ConfigParser()
     cp.read(os.path.expanduser('~/.concreate'))
     return cp._sections
-
-
-def download_file(url, destination):
-    """ Downloads a file from url and save it as destination """
-    logger.debug("Fetching '%s' as %s" % (url, destination))
-
-    parsedUrl = urlparse.urlparse(url)
-
-    if parsedUrl.scheme == 'file' or not parsedUrl.scheme:
-        if os.path.isdir(parsedUrl.path):
-            shutil.copytree(parsedUrl.path, destination)
-        else:
-            shutil.copy(parsedUrl.path, destination)
-    elif parsedUrl.scheme == 'http' or parsedUrl.scheme == 'https':
-        verify = cfg.get('common', {}).get('ssl_verify', True)
-        if str(verify).lower() == 'false':
-            verify = False
-
-        res = requests.get(url, verify=verify, stream=True)
-        if res.status_code != 200:
-            raise ConcreateError("Could not download file from %s" % url)
-        with open(destination, 'wb') as f:
-            for chunk in res.iter_content(chunk_size=1024):
-                f.write(chunk)
-    else:
-        raise ConcreateError("Unsupported URL scheme: %s" % (url))
-
-
-def prepare_external_repositories(image_dir):
-    """ Fetch repository definitions from provided urls """
-    added_repos = []
-    repo_file_urls = cfg.get('repository', {}).get('urls', None)
-    if not repo_file_urls:
-        return added_repos
-
-    target_dir = os.path.join(image_dir, 'repos')
-    os.makedirs(target_dir)
-
-    for url in repo_file_urls.split(','):
-        url = url.strip()
-        download_file(url, os.path.join(target_dir,
-                                        os.path.basename(url)))
-        added_repos.append(os.path.splitext(os.path.basename(url))[0])
-    return added_repos
 
 
 def cleanup(target):
