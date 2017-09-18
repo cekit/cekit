e60264754fb13bb7860a3f2a1eb1930a61440fa4
Marek Goldmann <mgoldman@redhat.com>
2017-09-18 14:44:02 +0200

    Fix issue when repositories are not defined

--- concreate/generator.py
+++ concreate/generator.py
@@ -121,7 +121,9 @@ class Generator(object):
         # We need to remove the custom "__name__" element before we can show
         # which repository keys are defined in the configuration
         configured_repository_names = configured_repositories.keys()
-        configured_repository_names.remove('__name__')
+
+        if '__name__' in configured_repository_names:
+            configured_repository_names.remove('__name__')
 
         added_repos = []
         target_dir = os.path.join(self.target, 'image', 'repos')
