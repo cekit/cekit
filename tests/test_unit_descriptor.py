cfe6055dff30705e9f05cbf5af2525dde5e053f2
David Becvarik <dbecvari@redhat.com>
2017-10-20 13:32:50 +0200

    Fixed defaults value handling

--- tests/test_unit_descriptor.py
+++ tests/test_unit_descriptor.py
@@ -1,75 +1,94 @@
-import os
-import tempfile
-import unittest
+import pytest
 import yaml
 
 from concreate import descriptor
 from concreate.errors import ConcreateError
 
 
-class TestDescriptor(unittest.TestCase):
+def test_descriptor_schema_version():
+    img_descriptor = descriptor.Descriptor.__new__(descriptor.Descriptor)
+    img_descriptor.descriptor = {'schema_version': 1}
+    img_descriptor.check_schema_version()
 
-    def test_descriptor_schema_version(self):
-        img_descriptor = descriptor.Descriptor.__new__(descriptor.Descriptor)
-        img_descriptor.descriptor = {'schema_version': 1}
+
+def test_descriptor_schema_version_bad_version():
+    img_descriptor = descriptor.Descriptor.__new__(descriptor.Descriptor)
+    img_descriptor.descriptor = {'schema_version': 123}
+    with pytest.raises(ConcreateError):
         img_descriptor.check_schema_version()
 
-    def test_descriptor_schema_version_bad_version(self):
-        img_descriptor = descriptor.Descriptor.__new__(descriptor.Descriptor)
-        img_descriptor.descriptor = {'schema_version': 123}
-        with self.assertRaises(ConcreateError):
-            img_descriptor.check_schema_version()
+
+def prepare_descriptor(path, data={}):
+    image = {'name': 'image/name', 'version': 1.0,
+             'from': 'from/image', 'schema_version': 1}
+    image.update(data)
+
+    with open(path, 'w') as fd:
+        yaml.dump(image, fd, default_flow_style=False)
+    return path
 
 
-class TestLabels(unittest.TestCase):
+def test_no_labels_should_be_added(tmpdir):
+    descriptor_path = prepare_descriptor(str(tmpdir.mkdir('image').join('image.yaml')))
 
-    def setUp(self):
-        _, self.descriptor = tempfile.mkstemp()
+    img_descriptor = descriptor.Descriptor(descriptor_path, 'image')
+    img_descriptor._process_labels()
 
-    def tearDown(self):
-        os.remove(self.descriptor)
+    assert img_descriptor.label('description') is None
+    assert img_descriptor.label('summary') is None
+    assert img_descriptor.label('maintainer') is None
 
-    def prepare_descriptor(self, data={}):
-        image = {'name': 'image/name', 'version': 1.0,
-                 'from': 'from/image', 'schema_version': 1}
-        image.update(data)
 
-        with open(self.descriptor, 'w') as outfile:
-            yaml.dump(image, outfile, default_flow_style=False)
+def test_description_label_should_be_added(tmpdir):
+    descriptor_path = prepare_descriptor(str(tmpdir.mkdir('image').join('image.yaml')),
+                                         {'description': 'This is image description'})
 
-    def test_no_labels_should_be_added(self):
-        self.prepare_descriptor()
+    img_descriptor = descriptor.Descriptor(descriptor_path, 'image')
+    img_descriptor._process_labels()
 
-        img_descriptor = descriptor.Descriptor(self.descriptor, 'image')
-        img_descriptor._process_labels()
+    assert img_descriptor.label('maintainer') is None
+    assert img_descriptor.label('description').get('value') == 'This is image description'
+    # In this case the 'summary' label should be also set
+    assert img_descriptor.label('summary').get('value') == 'This is image description'
 
-        self.assertIsNone(img_descriptor.label('description'))
-        self.assertIsNone(img_descriptor.label('summary'))
-        self.assertIsNone(img_descriptor.label('maintainer'))
 
-    def test_description_label_should_be_added(self):
-        self.prepare_descriptor({'description': 'This is image description'})
+def test_description_and_summary_labels_should_not_be_overriden(tmpdir):
+    descriptor_path = prepare_descriptor(str(tmpdir.mkdir('image').join('image.yaml')),
+                                         {'description': 'This is image description', 'labels': [
+                                             {'name': 'summary',
+                                              'value': 'summary value'},
+                                             {'name': 'description',
+                                              'value': 'description value'}]})
 
-        img_descriptor = descriptor.Descriptor(self.descriptor, 'image')
-        img_descriptor._process_labels()
+    img_descriptor = descriptor.Descriptor(descriptor_path, 'image')
+    img_descriptor._process_labels()
 
-        self.assertIsNone(img_descriptor.label('maintainer'))
-        self.assertEqual(img_descriptor.label('description').get(
-            'value'), 'This is image description')
-        # In this case the 'summary' label should be also set
-        self.assertEqual(img_descriptor.label('summary').get(
-            'value'), 'This is image description')
+    assert img_descriptor.label('maintainer') is None
+    assert img_descriptor.label('description').get('value') == 'description value'
+    assert img_descriptor.label('summary').get('value') == 'summary value'
 
-    def test_description_and_summary_labels_should_not_be_overriden(self):
-        self.prepare_descriptor({'description': 'This is image description', 'labels': [
-                                {'name': 'summary', 'value': 'summary value'},
-                                {'name': 'description', 'value': 'description value'}]})
 
-        img_descriptor = descriptor.Descriptor(self.descriptor, 'image')
-        img_descriptor._process_labels()
+def test_override_default_user_run(tmpdir):
+    descriptor_path = prepare_descriptor(str(tmpdir.mkdir('image').join('image.yaml')))
+    overrides_path = prepare_descriptor(str(tmpdir.mkdir('override').join('overrides.yaml')),
+                                        {'run': {'user': 123}})
 
-        self.assertIsNone(img_descriptor.label('maintainer'))
-        self.assertEqual(img_descriptor.label(
-            'description').get('value'), 'description value')
-        self.assertEqual(img_descriptor.label(
-            'summary').get('value'), 'summary value')
+    img_descriptor = descriptor.Descriptor(descriptor_path, 'image')
+    overrides_descriptor = descriptor.Descriptor(overrides_path, 'image')
+    overrides_descriptor.merge(img_descriptor)
+    overrides_descriptor.prepare_defaults()
+
+    assert overrides_descriptor.descriptor['run']['user'] == 123
+
+
+def test_override_default_user_kept(tmpdir):
+    descriptor_path = prepare_descriptor(str(tmpdir.mkdir('image').join('image.yaml')),
+                                         {'run': {'user': 123}})
+    overrides_path = prepare_descriptor(str(tmpdir.mkdir('override').join('overrides.yaml')))
+
+    img_descriptor = descriptor.Descriptor(descriptor_path, 'image')
+    overrides_descriptor = descriptor.Descriptor(overrides_path, 'image')
+    overrides_descriptor.merge(img_descriptor)
+    overrides_descriptor.prepare_defaults()
+
+    assert overrides_descriptor.descriptor['run']['user'] == 123
