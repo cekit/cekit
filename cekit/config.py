f14fcbaae63175a997e4940cba76922ff68e4c5b
David Becvarik <dbecvari@redhat.com>
2018-09-25 20:15:38 +0200

    removed code smell

--- cekit/config.py
+++ cekit/config.py
@@ -3,7 +3,7 @@ import yaml
 
 try:
     import ConfigParser as configparser
-except:
+except ImportError:
     import configparser
 
 
@@ -11,19 +11,22 @@ class Config(object):
     """Represents Cekit configuration - behaves as Singleton"""
     cfg = {}
 
-    def configure(self, config_path, cmdline_args):
-        self._load_cfg(config_path)
-        self._override_config(cmdline_args)
+    @classmethod
+    def configure(cls, config_path, cmdline_args):
+        cls._load_cfg(config_path)
+        cls._override_config(cmdline_args)
 
-    def _override_config(self, cmdline_args):
+    @classmethod
+    def _override_config(cls, cmdline_args):
         if isinstance(cmdline_args.get('redhat'), bool):
-            Config.cfg['common']['redhat'] = cmdline_args.get('redhat')
+            cls.cfg['common']['redhat'] = cmdline_args.get('redhat')
         if cmdline_args.get('work_dir'):
-            Config.cfg['common']['work_dir'] = cmdline_args.get('work_dir')
+            cls.cfg['common']['work_dir'] = cmdline_args.get('work_dir')
         if isinstance(cmdline_args.get('addhelp'), bool):
-            Config.cfg['doc']['addhelp'] = cmdline_args.get('addhelp')
+            cls.cfg['doc']['addhelp'] = cmdline_args.get('addhelp')
 
-    def _load_cfg(self, config_path):
+    @classmethod
+    def _load_cfg(cls, config_path):
         """ Loads configuration from cekit config file
 
         params:
@@ -31,18 +34,19 @@ class Config(object):
         """
         cp = configparser.ConfigParser()
         cp.read(os.path.expanduser(config_path))
-        Config.cfg = cp._sections
-        Config.cfg['common'] = Config.cfg.get('common', {})
-        Config.cfg['common']['work_dir'] = Config.cfg.get('common').get('work_dir', '~/.cekit')
-        Config.cfg['common']['redhat'] = yaml.safe_load(
-            Config.cfg.get('common', {}).get('redhat', 'False'))
+        cls.cfg = cp._sections
+        cls.cfg['common'] = cls.cfg.get('common', {})
+        cls.cfg['common']['work_dir'] = cls.cfg.get('common').get('work_dir', '~/.cekit')
+        cls.cfg['common']['redhat'] = yaml.safe_load(
+            cls.cfg.get('common', {}).get('redhat', 'False'))
 
-        Config.cfg['doc'] = Config.cfg.get('doc', {})
-        Config.cfg['doc']['addhelp'] = yaml.safe_load(
-            Config.cfg.get('doc').get('addhelp', 'False'))
+        cls.cfg['doc'] = cls.cfg.get('doc', {})
+        cls.cfg['doc']['addhelp'] = yaml.safe_load(
+            cls.cfg.get('doc').get('addhelp', 'False'))
 
-    def get(self, *args):
-        value = Config.cfg
+    @classmethod
+    def get(cls, *args):
+        value = cls.cfg
         for arg in args:
             value = value.get(arg)
         return value
