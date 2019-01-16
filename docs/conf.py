e0f8843bac56e9bd456c3ed0349e73f456bf3f0f
Marek Goldmann <mgoldman@redhat.com>
2019-01-16 14:39:39 +0100

    Make documentation prettier

--- docs/conf.py
+++ docs/conf.py
@@ -91,6 +91,15 @@ html_theme = 'alabaster'
 #
 # html_theme_options = {}
 
+html_theme_options = {
+    'logo': 'logo-128.png',
+    'github_user': 'cekit',
+    'github_repo': 'cekit',
+    'description': 'Container image creation tool',
+    'fixed_sidebar': True,
+    'sidebar_width': '230px',
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
