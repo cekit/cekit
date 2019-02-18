eeddf8c1b290215fc0a4df47a448aa04b97b6aaa
Marek Goldmann <mgoldman@redhat.com>
2019-02-18 12:53:18 +0100

    Update tests after changing the tasks are kicked in OSBS

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -3,6 +3,11 @@ import os
 import subprocess
 import yaml
 
+try:
+    from unittest.mock import call
+except ImportError:
+    from mock import call
+
 from cekit.builder import Builder
 
 
@@ -12,7 +17,9 @@ def test_osbs_builder_defaults(mocker):
     builder = Builder('osbs', 'tmp', {})
 
     assert builder._release is False
-    assert builder._rhpkg == 'fedpkg'
+    assert builder._fedpkg == 'fedpkg'
+    assert builder._koji == 'koji'
+    assert builder._koji_url == 'https://koji.fedoraproject.org/koji'
     assert builder._nowait is False
 
 
@@ -21,7 +28,9 @@ def test_osbs_builder_redhat(mocker):
 
     builder = Builder('osbs', 'tmp', {'redhat': True})
 
-    assert builder._rhpkg == 'rhpkg'
+    assert builder._fedpkg == 'rhpkg'
+    assert builder._koji == 'brew'
+    assert builder._koji_url == 'https://brewweb.engineering.redhat.com/brew'
 
 
 def test_osbs_builder_use_rhpkg_staget(mocker):
@@ -31,7 +40,9 @@ def test_osbs_builder_use_rhpkg_staget(mocker):
               'redhat': True}
     builder = Builder('osbs', 'tmp', params)
 
-    assert builder._rhpkg == 'rhpkg-stage'
+    assert builder._fedpkg == 'rhpkg-stage'
+    assert builder._koji == 'brew-stage'
+    assert builder._koji_url == 'https://brewweb.stage.engineering.redhat.com/brew'
 
 
 def test_osbs_builder_custom_commit_msg(mocker):
@@ -116,8 +127,7 @@ class DistGitMock(object):
         pass
 
 
-def create_osbs_build_object(mocker, builder_type, params):
-    mocker.patch.object(subprocess, 'check_output')
+def create_osbs_build_object(mocker, builder_type, params={}):
     mocker.patch('cekit.tools.decision')
 
     builder = Builder(builder_type, 'tmp', params)
@@ -127,61 +137,109 @@ def create_osbs_build_object(mocker, builder_type, params):
     return builder
 
 
-def test_osbs_builder_run_rhpkg_stage(mocker):
-    mocker.patch.object(subprocess, 'check_output')
-
+def test_osbs_builder_run_brew_stage(mocker):
     params = {'stage': True,
               'redhat': True}
 
-    check_call = mocker.patch.object(subprocess, 'check_call')
+    check_output = mocker.patch.object(subprocess, 'check_output', autospec=True, side_effect=[
+                                       b'ssh://user:password@something.redhat.com/containers/openjdk', b'c5a0731b558c8a247dd7f85b5f54462cd5b68b23', b'12345'])
     builder = create_osbs_build_object(mocker, 'osbs', params)
+    mocker.patch.object(builder, '_wait_for_osbs_task')
+    builder.dist_git.branch = "some-branch"
     builder.build()
 
-    check_call.assert_called_once_with(['rhpkg-stage', 'container-build', '--scratch'])
+    check_output.assert_has_calls([
+        call(['git', 'remote', 'get-url', 'origin']),
+        call(['git', 'rev-parse', 'HEAD']),
+        call(['brew-stage', 'call', '--python', 'buildContainer', '--kwargs',
+              "{'src': 'git://something.redhat.com/containers/openjdk#c5a0731b558c8a247dd7f85b5f54462cd5b68b23', 'target': 'some-branch-containers-candidate', 'opts': {'scratch': True, 'git_branch': 'some-branch', 'yum_repourls': []}}"])
+    ])
+
+    builder._wait_for_osbs_task.assert_called_once_with('12345')
 
 
-def test_osbs_builder_run_rhpkg(mocker):
-    mocker.patch.object(subprocess, 'check_output')
+def test_osbs_builder_run_brew(mocker):
+    params = {'redhat': True}
 
-    check_call = mocker.patch.object(subprocess, 'check_call')
-    builder = create_osbs_build_object(mocker, 'osbs', {'redhat': True})
+    check_output = mocker.patch.object(subprocess, 'check_output', autospec=True, side_effect=[
+                                       b'ssh://user:password@something.redhat.com/containers/openjdk', b'c5a0731b558c8a247dd7f85b5f54462cd5b68b23', b'12345'])
+    builder = create_osbs_build_object(mocker, 'osbs', params)
+    mocker.patch.object(builder, '_wait_for_osbs_task')
+    builder.dist_git.branch = "some-branch"
     builder.build()
 
-    check_call.assert_called_once_with(['rhpkg', 'container-build', '--scratch'])
+    check_output.assert_has_calls([
+        call(['git', 'remote', 'get-url', 'origin']),
+        call(['git', 'rev-parse', 'HEAD']),
+        call(['brew', 'call', '--python', 'buildContainer', '--kwargs',
+              "{'src': 'git://something.redhat.com/containers/openjdk#c5a0731b558c8a247dd7f85b5f54462cd5b68b23', 'target': 'some-branch-containers-candidate', 'opts': {'scratch': True, 'git_branch': 'some-branch', 'yum_repourls': []}}"])
+    ])
+
+    builder._wait_for_osbs_task.assert_called_once_with('12345')
 
 
-def test_osbs_builder_run_rhpkg_nowait(mocker):
-    mocker.patch.object(subprocess, 'check_output')
+def test_osbs_builder_run_koji(mocker):
+    params = {'redhat': False}
+
+    check_output = mocker.patch.object(subprocess, 'check_output', autospec=True, side_effect=[
+                                       b'ssh://user:password@something.redhat.com/containers/openjdk', b'c5a0731b558c8a247dd7f85b5f54462cd5b68b23', b'12345'])
+    builder = create_osbs_build_object(mocker, 'osbs', params)
+    mocker.patch.object(builder, '_wait_for_osbs_task')
+    builder.dist_git.branch = "some-branch"
+    builder.build()
+
+    check_output.assert_has_calls([
+        call(['git', 'remote', 'get-url', 'origin']),
+        call(['git', 'rev-parse', 'HEAD']),
+        call(['koji', 'call', '--python', 'buildContainer', '--kwargs',
+              "{'src': 'git://something.redhat.com/containers/openjdk#c5a0731b558c8a247dd7f85b5f54462cd5b68b23', 'target': 'some-branch-containers-candidate', 'opts': {'scratch': True, 'git_branch': 'some-branch', 'yum_repourls': []}}"])
+    ])
+
+    builder._wait_for_osbs_task.assert_called_once_with('12345')
+
+
+def test_osbs_builder_run_brew_nowait(mocker):
     params = {'nowait': True,
               'redhat': True}
 
-    check_call = mocker.patch.object(subprocess, 'check_call')
+    check_output = mocker.patch.object(subprocess, 'check_output', autospec=True, side_effect=[
+                                       b'ssh://user:password@something.redhat.com/containers/openjdk', b'c5a0731b558c8a247dd7f85b5f54462cd5b68b23', b'12345'])
     builder = create_osbs_build_object(mocker, 'osbs', params)
+    mocker.patch.object(builder, '_wait_for_osbs_task')
+    builder.dist_git.branch = "some-branch"
     builder.build()
 
-    check_call.assert_called_once_with(['rhpkg', 'container-build', '--nowait', '--scratch'])
+    builder._wait_for_osbs_task.assert_not_called()
 
 
-def test_osbs_builder_run_rhpkg_user(mocker):
+def test_osbs_builder_run_brew_user(mocker):
     params = {'user': 'Foo',
               'redhat': True}
 
-    check_call = mocker.patch.object(subprocess, 'check_call')
+    check_output = mocker.patch.object(subprocess, 'check_output', autospec=True, side_effect=[
+                                       b'ssh://user:password@something.redhat.com/containers/openjdk', b'c5a0731b558c8a247dd7f85b5f54462cd5b68b23', b'12345'])
     builder = create_osbs_build_object(mocker, 'osbs', params)
+    mocker.patch.object(builder, '_wait_for_osbs_task')
+    builder.dist_git.branch = "some-branch"
     builder.build()
 
-    check_call.assert_called_once_with(['rhpkg', '--user', 'Foo', 'container-build', '--scratch'])
+    check_output.assert_called_with(['brew', '--user', 'Foo', 'call', '--python', 'buildContainer', '--kwargs',
+                                     "{'src': 'git://something.redhat.com/containers/openjdk#c5a0731b558c8a247dd7f85b5f54462cd5b68b23', 'target': 'some-branch-containers-candidate', 'opts': {'scratch': True, 'git_branch': 'some-branch', 'yum_repourls': []}}"])
 
 
-def test_osbs_builder_run_rhpkg_target(mocker):
+def test_osbs_builder_run_brew_target(mocker):
     params = {'target': 'Foo',
               'redhat': True}
 
-    check_call = mocker.patch.object(subprocess, 'check_call')
+    check_output = mocker.patch.object(subprocess, 'check_output', autospec=True, side_effect=[
+                                       b'ssh://user:password@something.redhat.com/containers/openjdk', b'c5a0731b558c8a247dd7f85b5f54462cd5b68b23', b'12345'])
     builder = create_osbs_build_object(mocker, 'osbs', params)
+    mocker.patch.object(builder, '_wait_for_osbs_task')
+    builder.dist_git.branch = "some-branch"
     builder.build()
 
-    check_call.assert_called_once_with(['rhpkg', 'container-build', '--target', 'Foo', '--scratch'])
+    check_output.assert_called_with(['brew', 'call', '--python', 'buildContainer', '--kwargs',
+                                     "{'src': 'git://something.redhat.com/containers/openjdk#c5a0731b558c8a247dd7f85b5f54462cd5b68b23', 'target': 'Foo', 'opts': {'scratch': True, 'git_branch': 'some-branch', 'yum_repourls': []}}"])
 
 
 def test_docker_builder_defaults():
