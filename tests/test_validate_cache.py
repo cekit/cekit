c7e391ba1b11aea1f7aae522f35d7e2fc16f3011
Marek Goldmann <mgoldman@redhat.com>
2019-02-27 10:32:58 +0100

    Rewrite cekit-cache CLI to click

--- tests/test_validate_cache.py
+++ tests/test_validate_cache.py
@@ -1,129 +1,144 @@
 import os
+import re
 import pytest
 import sys
 
-from cekit.cache.cli import CacheCli
+from cekit.cache.cli import cli
+from cekit.crypto import SUPPORTED_HASH_ALGORITHMS
+
+from click.testing import CliRunner
 
 
-def test_cekit_cache_ls(mocker, tmpdir, capsys):
+def test_cekit_cache_ls(mocker, tmpdir):
     work_dir = str(tmpdir.mkdir('work_dir'))
-    mocker.patch.object(sys, 'argv', ['cekit-cache',
-                                      '--work-dir',
-                                      work_dir,
-                                      '-v',
-                                      'ls'])
-    run_cekit_cache()
-    out, err = capsys.readouterr()
-    assert 'No artifacts cached!' in out
+
+    result = run_cekit_cache(['-v',
+                              '--work-dir',
+                              work_dir,
+                              'ls'])
+
+    assert 'No artifacts cached!' in result.output
 
 
 def test_cekit_cache_rm_non_existing(mocker, tmpdir):
     work_dir = str(tmpdir.mkdir('work_dir'))
-    mocker.patch.object(sys, 'argv', ['cekit-cache',
-                                      '--work-dir',
-                                      work_dir,
-                                      '-v',
-                                      'rm',
-                                      '12345'])
-    run_cekit_cache(1)
+    run_cekit_cache(['-v',
+                     '--work-dir',
+                     work_dir,
+                     'rm',
+                     '12345'], 1)
 
 
-def test_cekit_cache_add_artifact(mocker, tmpdir, capsys):
-    work_dir = str(tmpdir.mkdir('work_dir'))
-    out, err = capsys.readouterr()
-
-    artifact = os.path.join(work_dir, 'artifact')
-    open(artifact, 'a').close()
-
-    mocker.patch.object(sys, 'argv', ['cekit-cache',
-                                      '--work-dir',
-                                      work_dir,
-                                      '-v',
-                                      'add',
-                                      artifact,
-                                      '--md5',
-                                      'd41d8cd98f00b204e9800998ecf8427e'])
-    run_cekit_cache(0)
-
-
-def test_cekit_cache_add_artifact_existing(mocker, tmpdir, capsys):
+def test_cekit_cache_add_artifact(mocker, tmpdir):
     work_dir = str(tmpdir.mkdir('work_dir'))
 
     artifact = os.path.join(work_dir, 'artifact')
     open(artifact, 'a').close()
-    mocker.patch.object(sys, 'argv', ['cekit-cache',
-                                      '--work-dir',
-                                      work_dir,
-                                      '-v',
-                                      'add',
-                                      artifact,
-                                      '--md5',
-                                      'd41d8cd98f00b204e9800998ecf8427e'])
-    run_cekit_cache(0)
-    run_cekit_cache(0)
-    out, err = capsys.readouterr()
-    assert 'Artifact is already cached!' in out
+
+    result = run_cekit_cache(['-v',
+                              '--work-dir',
+                              work_dir,
+                              'add',
+                              artifact,
+                              '--md5',
+                              'd41d8cd98f00b204e9800998ecf8427e'])
+
+    assert "cached with UUID" in result.output
 
 
-def test_cekit_cache_delete_artifact(mocker, tmpdir, capsys):
+def test_cekit_cache_add_artifact_existing(mocker, tmpdir):
     work_dir = str(tmpdir.mkdir('work_dir'))
 
     artifact = os.path.join(work_dir, 'artifact')
     open(artifact, 'a').close()
-    mocker.patch.object(sys, 'argv', ['cekit-cache',
-                                      '--work-dir',
-                                      work_dir,
-                                      '-v',
-                                      'add',
-                                      artifact,
-                                      '--md5',
-                                      'd41d8cd98f00b204e9800998ecf8427e'])
-    run_cekit_cache(0)
-    out, err = capsys.readouterr()
-    artifact_id = out[:-1]
-    print('Removing artifact: "%s"' % out)
 
-    mocker.patch.object(sys, 'argv', ['cekit-cache',
-                                      '--work-dir',
-                                      work_dir,
-                                      '-v',
-                                      'rm',
-                                      artifact_id])
+    args = ['-v',
+            '--work-dir',
+            work_dir,
+            'add',
+            artifact,
+            '--md5',
+            'd41d8cd98f00b204e9800998ecf8427e']
 
-    run_cekit_cache(0)
+    run_cekit_cache(args)
+    result = run_cekit_cache(args)
+
+    assert 'is already cached!' in result.output
 
 
-def test_cekit_cache_delete_not_existing_artifact(mocker, tmpdir, capsys):
+def test_cekit_cache_delete_artifact(mocker, tmpdir):
     work_dir = str(tmpdir.mkdir('work_dir'))
-    mocker.patch.object(sys, 'argv', ['cekit-cache',
-                                      '--work-dir',
-                                      work_dir,
-                                      '-v',
-                                      'rm',
-                                      'foo'])
+    artifact = os.path.join(work_dir, 'artifact')
+    open(artifact, 'a').close()
 
-    run_cekit_cache(1)
-    out, err = capsys.readouterr()
-    assert "Artifact doesn't exists" in out
+    result = run_cekit_cache(['-v',
+                              '--work-dir',
+                              work_dir,
+                              'add',
+                              artifact,
+                              '--md5',
+                              'd41d8cd98f00b204e9800998ecf8427e'])
+
+    artifact_uuid = re.search(r'\'(.*)\'$', result.output).group(1)
+
+    run_cekit_cache(['-v',
+                     '--work-dir',
+                     work_dir,
+                     'rm',
+                     artifact_uuid])
 
 
-def test_cekit_cannot_add_artifact_without_checksum(mocker, tmpdir, capsys):
+def test_cekit_cache_delete_not_existing_artifact(mocker, tmpdir):
+    work_dir = str(tmpdir.mkdir('work_dir'))
+
+    result = run_cekit_cache(['-v',
+                              '--work-dir',
+                              work_dir,
+                              'rm',
+                              'foo'], 1)
+    assert "Artifact with UUID 'foo' doesn't exists in the cache" in result.output
+
+
+def test_cekit_cannot_add_artifact_without_checksum(mocker, tmpdir):
+    work_dir = str(tmpdir.mkdir('work_dir'))
+    artifact = os.path.join(work_dir, 'artifact')
+    open(artifact, 'a').close()
+
+    result = run_cekit_cache(['--work-dir',
+                              work_dir,
+                              'add',
+                              artifact], 2)
+
+    assert "At least one checksum must be provided" in result.output
+
+
+@pytest.mark.parametrize('algorithm', SUPPORTED_HASH_ALGORITHMS)
+def test_cekit_supported_algorithms(mocker, tmpdir, algorithm):
+    """
+    This is a bit counter intuitive, but we try to cache an artifact
+    with all known supported hash algorithms. In case when the algorithm
+    would not be supported, it would result in return code 2 and a different error message
+    """
+
     work_dir = str(tmpdir.mkdir('work_dir'))
 
     artifact = os.path.join(work_dir, 'artifact')
     open(artifact, 'a').close()
-    mocker.patch.object(sys, 'argv', ['cekit-cache',
-                                      '--work-dir',
-                                      work_dir,
-                                      'add',
-                                      artifact])
 
-    run_cekit_cache(1)
-    out, err = capsys.readouterr()
-    assert "Cannot cache Artifact without checksum" in out
+    result = run_cekit_cache(['-v',
+                              '--work-dir',
+                              work_dir,
+                              'add',
+                              artifact,
+                              "--{}".format(algorithm),
+                              'WRONG!'], 1)
+
+    assert "Cannot cache artifact {}: Artifact checksum verification failed!".format(
+        artifact) in result.output
 
 
-def run_cekit_cache(rc=0):
-    with pytest.raises(SystemExit) as system_exit:
-        CacheCli().parse().run()
-    assert system_exit.value.code == rc
+def run_cekit_cache(args, rc=0):
+    result = CliRunner().invoke(cli, args, catch_exceptions=False)
+    assert result.exit_code == rc
+
+    return result
