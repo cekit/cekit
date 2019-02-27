d9ae40de3714a8a60903e70b97d150ce149aea07
Marek Goldmann <mgoldman@redhat.com>
2019-02-27 14:44:54 +0100

    Use Map from tools, small tweaks

--- cekit/cli.py
+++ cekit/cli.py
@@ -12,7 +12,7 @@ from cekit.generator.base import Generator
 from cekit.log import setup_logging
 from cekit.test.collector import TestCollector
 from cekit.test.runner import TestRunner
-from cekit.tools import DependencyHandler, cleanup
+from cekit.tools import DependencyHandler, Map, cleanup
 from cekit.version import version
 
 # FIXME we should try to move this to json
@@ -22,10 +22,10 @@ config = Config()
 
 
 @click.group(context_settings=dict(max_content_width=100))
-@click.option('--descriptor', metavar="<descriptor>", help="Path to image descriptor file.", default="image.yaml", show_default=True)
+@click.option('--descriptor', metavar="PATH", help="Path to image descriptor file.", default="image.yaml", show_default=True)
 @click.option('-v', '--verbose', help="Enable verbose output.", is_flag=True)
 @click.option('--work-dir', help="Location of the working directory.", default="~/.cekit", show_default=True)
-@click.option('--config', help="Path to configuration file.", default="~/.cekit/config", show_default=True)
+@click.option('--config', metavar="PATH", help="Path to configuration file.", default="~/.cekit/config", show_default=True)
 @click.option('--redhat', help="Set default options for Red Hat internal infrastructure.", is_flag=True)
 @click.option('--target', metavar="PATH", help="Path to directory where files should be generated", default="target", show_default=True)
 # TODO: Remove this option
@@ -207,12 +207,6 @@ def run():
     # return Map({'commands': commands, 'params': Map(params)})
 
     Cekit(commands, Map(params)).run()
-
-
-class Map(dict):
-    __getattr__ = dict.get
-    __setattr__ = dict.__setitem__
-    __delattr__ = dict.__delitem__
 
 
 class Cekit(object):
