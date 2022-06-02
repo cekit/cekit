97e14726bc2bcd9032ea87cdc4e3bb253c06fc4c
Andy Green <agreen@atlassian.com>
2022-06-02 12:57:51 -0600

    #780: Add test showing issue with missing tests.

---
+++ tests/images/module-tests/modules/mymodules/tests/features/mymodule.feature
@@ -0,0 +1,6 @@
+@dummy
+Feature: my module
+
+  Scenario: Test feature in module
+    Given image is built
+    Then the image should contain label io.cekit.version
--- tests/test_integ_test_behave.py
+++ tests/test_integ_test_behave.py
@@ -149,3 +149,38 @@ Feature: Basic tests
     assert "3 steps passed, 0 failed, 0 skipped, 0 undefined" in result.output
 
     shutil.rmtree(os.path.join(test_image_dir, "target"), ignore_errors=True)
+
+
+def test_execute_behave_test_from_module():
+
+    # given: (image is built)
+    image_dir = os.path.join(tempfile.mkdtemp(prefix="tmp-cekit-test"), "project")
+
+    shutil.copytree(
+        os.path.join(os.path.dirname(__file__), "images", "module-tests"), image_dir
+    )
+
+    with Chdir(image_dir):
+        build_result = CliRunner().invoke(
+            cli, ["-v", "build", "docker"], catch_exceptions=False
+        )
+
+    sys.stdout.write("\n")
+    sys.stdout.write(build_result.output)
+
+    assert build_result.exit_code == 0
+
+    # when: tests are run
+    with Chdir(image_dir):
+        test_result = CliRunner().invoke(
+            cli, ["-v", "test", "behave"], catch_exceptions=False
+        )
+
+    sys.stdout.write("\n")
+    sys.stdout.write(test_result.output)
+
+    # then:
+    assert test_result.exit_code == 0
+    assert "1 feature passed, 0 failed, 0 skipped" in test_result.output
+    assert "1 scenario passed, 0 failed, 0 skipped" in test_result.output
+    assert "2 steps passed, 0 failed, 0 skipped, 0 undefined" in test_result.output
